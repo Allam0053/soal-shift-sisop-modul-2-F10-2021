aa38cf7522b55baff29370a21b4e2e0d766aa926
rizadwiandhika <rizadwiandhika@gmail.com>
2021-04-15 23:22:13 +0700

    done: 3b

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -4,10 +4,9 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <curl/curl.h>
 #include <string.h>
 
-void get_timestamp(char time_stamp[]);
+long get_timestamp(char time_stamp[]);
 
 int main() {
   pid_t pid = fork();
@@ -15,7 +14,7 @@ int main() {
   if (pid < 0) exit(EXIT_FAILURE);
   if (pid > 0) exit(EXIT_SUCCESS);
 
-  /* Below here is child process */
+  /* Below here is child (daemon) process */
 
   umask(0);
   pid_t sid = setsid();
@@ -28,14 +27,52 @@ int main() {
   close(STDERR_FILENO);
   
   while (1) {
-    pid_t pid_in_daemon = fork();
+    pid_t pid_create_dir = fork();
+    char dir_timestamp[20];
+    get_timestamp(dir_timestamp);
 
-    if (pid_in_daemon != 0) {
-      char time_stamp[20] = "";
-      get_timestamp(time_stamp);
-      char* argv[] = { "mkdir", "-p", time_stamp };
+    if (pid_create_dir == 0) {
+      char* argv[] = { "mkdir", "-p", dir_timestamp, NULL };
       execv("/bin/mkdir", argv);
     }
+
+    while (wait(NULL) > 0);
+
+    /* Pembuatan folder telah selesai */
+
+    pid_t pid_download_pictures = fork();
+
+    if (pid_download_pictures == 0) {
+      int downloaded_pictures = 0;
+
+      while (downloaded_pictures++ < 10) {
+        pid_t pid_for_download = fork();
+
+        if (pid_for_download == 0) {
+          char pic_timestamp[20];
+          int time = (int) get_timestamp(pic_timestamp);
+          int size = 50 + (time % 1000);
+          char pic_path_dest[100];
+          char url[27];
+
+          strcat(pic_path_dest, dir_timestamp);
+          strcat(pic_path_dest, "/");
+          strcat(pic_path_dest, pic_timestamp);
+          strcat(pic_path_dest, ".jpg");
+
+          sprintf(url, "https://picsum.photos/%d", size);
+
+          char* argv[] = { "wget", "-O", pic_path_dest, url };
+          // ! Path wget harus diubah
+          execv("/usr/local/bin/wget", argv);
+        }
+
+        sleep(5);
+      }
+
+    }
+
+    /* Dibawah sini gak boleh ada wait (kalo ada wait, berarti dia nunggu si download. jadinya 90 detik) */
 
     sleep(40);
   }
@@ -43,7 +80,7 @@ int main() {
   return 0;
 }
 
-void get_timestamp(char time_stamp[]) {
+long get_timestamp(char time_stamp[]) {
   time_t rawtime = time(NULL);
   if (rawtime == -1) return;
 
@@ -76,4 +113,6 @@ void get_timestamp(char time_stamp[]) {
 
   sprintf(temp, "%02d", ptm->tm_sec);
   strcat(time_stamp, temp);
+
+  return rawtime;
 }
