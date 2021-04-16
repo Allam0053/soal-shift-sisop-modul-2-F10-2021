1c6a66be6772e3a304492e19a6dcbbd078b1f365
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-04-16 13:15:01 +0700

    Update soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -10,7 +10,10 @@
 
 int main()
 {
+  
   pid_t pid, sid;
+  
+  
   pid = fork();
   if(pid < 0)
     exit(EXIT_FAILURE);
@@ -42,5 +45,53 @@ int main()
     //https://drive.google.com/file/d/1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp/view
     execv("/bin/wget", arg3);
   }
-
+while((wait(&status)) > 0);
+  pid = fork();
+  if(pid < 0)
+    exit(EXIT_FAILURE);
+  if(pid == 0){
+    char *arg4[] = {"unzip", "Foto.zip", NULL};
+    execv("/bin/unzip", arg4);
+  }
+ while((wait(&status)) > 0);
+  pid = fork();
+  if(pid < 0)
+    exit(EXIT_FAILURE);
+  if(pid == 0){
+    char *arg5[] = {"unzip", "Musik.zip", NULL};
+    execv("/bin/unzip", arg5);
+  }
+ while((wait(&status)) > 0);
+  pid = fork();
+  if(pid < 0)
+    exit(EXIT_FAILURE);
+  if(pid == 0){
+    char *arg6[] = {"unzip", "Film.zip", NULL};
+    execv("/bin/unzip", arg6);
+  }
+ 
+  while((wait(&status)) > 0);
+  pid = fork();
+  if(pid < 0)
+    exit(EXIT_FAILURE);
+  if(pid == 0){
+    char *arg7[] = {"mv", "/home/dewangga99/FOTO", "/home/dewangga99/Pyoto", NULL};
+    execv("/bin/mv", arg7);
+  }
+  while((wait(&status)) > 0);
+  pid = fork();
+  if(pid < 0)
+    exit(EXIT_FAILURE);
+  if(pid == 0){
+    char *arg8[] = {"mv", "/home/dewangga99/MUSIK", "/home/dewangga99/Myusik", NULL};
+    execv("/bin/mv", arg8);
+  }
+  while((wait(&status)) > 0);
+  pid = fork();
+  if(pid < 0)
+    exit(EXIT_FAILURE);
+  if(pid == 0){
+    char *arg9[] = {"mv", "/home/dewangga99/FILM", "/home/dewangga99/Fylm NULL};
+    execv("/bin/mv", arg9);
+  }
 }
