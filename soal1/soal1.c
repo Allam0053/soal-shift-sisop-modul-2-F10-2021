0203d60502632627d312ea56f684c3422896fcd7
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2021-04-16 07:52:39 +0700

    placeholder sementara

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <syslog.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 
+int main()
+{
+  pid_t pid, sid;
+  pid = fork();
+  
+}
